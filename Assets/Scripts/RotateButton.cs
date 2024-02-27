8b6d140c54dfa38cd5733ae5b3342a10ce6c9446
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2024-02-26 20:52:50 -0600

    Created button prefab, sprite, and mono functionality.

---
+++ Assets/Scripts/RotateButton.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RotateButton : MonoBehaviour
+{
+     // Materials for color swapping
+     [SerializeField]
+     private Material defaultMat;
+     [SerializeField]
+     private Material hoverMat;
+     [SerializeField]
+     private Material pressedMat;
+
+     private Renderer m_Renderer;
+
+     private bool isHovering = false, isPressed = false;
+
+     
+     private void Awake()
+     {
+          // Get button's sprite renderer
+          m_Renderer = GetComponentInChildren<Renderer>();
+     }
+
+     // If the button has not been pressed, hovering will apply hovering material
+     // If the button was pressed, exits and re-enters while holding the mouse, it will stay as pressed material
+     private void OnMouseEnter()
+     {
+          m_Renderer.material = isPressed ? pressedMat : hoverMat;
+          isHovering = true;
+     }
+
+     // If the button is pressed on mouse exiting collider, it will stay pressed until mouse button is released
+     // If the button is not pressed, it will apply the default material
+     private void OnMouseExit()
+     {
+          m_Renderer.material = isPressed ? pressedMat : defaultMat;
+          isHovering = false;
+     }
+
+     // On pressing the button, the pressed material is applied
+     private void OnMouseDown()
+     {
+          m_Renderer.material = pressedMat;
+          isPressed = true;
+     }
+
+     // If the mouse is released while hovering, the hover material is applied, other wise the default material is applied
+     private void OnMouseUp()
+     {
+          m_Renderer.material = isHovering ? hoverMat : defaultMat;
+          isPressed = false;
+     }
+
+}
